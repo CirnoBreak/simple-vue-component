e6c77254cd849efb5e7321457cfeb16997b41ef9
CirnoBreak <1103054869@qq.com>
2018-12-30 20:49:46 +0800

    init

---
+++ src/components/form/form-item.vue
@@ -0,0 +1,126 @@
+<template>
+  <div>
+    <label v-if="label" :class="{ 'i-form-item-label-required': isRequired }">{{ label }}</label>
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import Emitter from '../../mixins/emitter.js'
+import AsyncValidator from 'async-validator'
+
+  export default {
+    name: 'iFormItem',
+    mixins: [Emitter],
+    inject: ['form'],
+    props: {
+      label: {
+        type: String,
+        default: ''
+      },
+      prop: {
+        type: String
+      }
+    },
+    data () {
+      return {
+        isRequired: false, // 是否为必填
+        validateState: '', // 校验状态
+        validateMessage: '' // 校验不通过的提示信息
+      }
+    },
+    computed: {
+      fieldValue () {
+        return this.form.model[this.prop]
+      }
+    },
+    methods: {
+      setRules () {
+        let rules = this.getRules()
+        if (rules.length) {
+          // 如果当前校验规则中有必填项则标记出来
+          rules.every((rule) => {
+            this.isRequired = rule.required
+          })
+        }
+        this.$on('on-form-blur', this.onFieldBlur)
+        this.$on('on-form-change', this.onFieldChange)
+      },
+      // 从Form的rules属性中获取当前FormItem的校验规则
+      getRules () {
+        let formRules = this.form.rules
+
+        formRules = formRules ? formRules[this.prop] : []
+
+        return [].concat(formRules || [])
+      },
+      // 重置数据
+      resetField () {
+        this.validateState = ''
+        this.validateMessage = ''
+
+        this.form.model[this.prop] = this.initialValue
+      },
+      getFileredRule (trigger) {
+        const rules = this.getRules()
+        return rules.filter(rule => !rule.trigger || rule.trigger.indexOf(trigger) !== -1)
+      },
+      validate(trigger, callback = function () {}) {
+        let rules = this.getFileredRule(trigger)
+
+        if (!rules || rules.length === 0) {
+          return true
+        }
+
+        this.validateState = 'validating'
+
+        let descriptor = {}
+        descriptor[this.prop] = rules
+
+        const validator = new AsyncValidator(descriptor)
+        let model = {}
+
+        model[this.prop] = this.fieldValue
+
+        validator.validate(model, { firstField: true }, errors => {
+          this.validateState = !errors ? 'success' : 'error'
+          this.validateMessage = errors ? errors[0].message : ''
+
+          callback(this.validateMessage)
+        })
+      },
+      onFieldBlur () {
+        this.validate('blur')
+      },
+      onFieldChange () {
+        this.validate('change')
+      }
+    },
+    mounted () {
+      // 若无prop传入则无需校验，也就无需缓存
+      if (this.prop) {
+        this.dispatch('iForm', 'on-form-item-add', this)
+
+        // 设置初始值，以便在重置时恢复默认值
+        this.initialValue = this.fieldValue
+
+        this.setRules()
+      }
+    },
+    beforeDestroy () {
+      this.dispatch('iForm', 'on-form-item-remove', this)
+    }
+  }
+</script>
+
+<style>
+.i-form-item-label-required:before {
+  content: '*';
+  color: red;
+}
+.i-form-item-message {
+  color: red;
+}
+</style>
---
+++ src/mixins/emitter.js
@@ -0,0 +1,35 @@
+function broadcat(componentName, eventName, params) {
+  this.$children.forEach(child => {
+    const name = child.$options.name;
+
+    if (name === componentName) {
+      child.$emit.apply(child, [eventName].concat(params));
+    } else {
+      broadcat.apply(child, [componentName, eventName].concat([params]));
+    }
+  })
+}
+
+export default {
+  methods: {
+    dispatch(componentName, eventName, params) {
+      let parent = this.$parent || this.$root;
+      let name = parent.$options.name;
+
+      while (parent && (!name || name !== componentName)) {
+        parent = parent.$parent;
+
+        if (parent) {
+          name = parent.$options.name;
+        }
+
+      }
+      if (parent) {
+        parent.$emit.apply(parent, [eventName].concat(params));
+      }
+    },
+    broadcat(componentName, eventName, params) {
+      broadcat.call(this, componentName, eventName, params);
+    }
+  }
+}
